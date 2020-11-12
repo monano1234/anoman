14267dcbcf929b1fe2372f96dcdb65f5ef9dcff2
monano1234 <74228831+monano1234@users.noreply.github.com>
2020-11-12 21:19:16 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+import TicTacToe from "./TicTacToe";
+import "papercss/dist/paper.min.css";
+
+function App() {
+    return ( <
+        >
+        <
+        Main >
+        <
+        TicTacToe / >
+        <
+        /Main> <
+        Footer >
+        <
+        FooterInner >
+        View the code on { " " } <
+        a href = "https://github.com/Clarity-89/React_tic_tac_toe" > Github < /a> <
+        /FooterInner>
+        Anom Purboyo_1119101738 { "" } <
+        /Footer> <
+        />
+    );
+}
+
+const Main = styled.main `
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex: 1 0 auto;
+`;
+
+const Footer = styled.footer `
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  flex: 0 0 auto;
+`;
+
+const FooterInner = styled.div `
+  padding: 16px 0;
+`;
+export default App;
